b58c315e98fb31eeb6ec9e49899deeda70c2a636
s0911602565 <s0911602565@gmail.com>
2023-11-09 23:39:11 +0800

    20231109_1139

--- src/main/java/com/example/abc/controller/MyController2.java
+++ src/main/java/com/example/abc/controller/MyController2.java
@@ -1,21 +1,43 @@
 package com.example.abc.controller;
 
 import com.example.abc.model.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class MyController2 {
-    /* 127.0.0.1:8080/c2/e1 */
-    @RequestMapping("e1")
+    /* 127.0.0.1:8080/ex1 */
+    @RequestMapping("ex1")
     public ModelAndView getMessage1(){
         User obj = new User();
         obj.setPwd("abc123");
         obj.setUsername("stephen");
-        ModelAndView m = new ModelAndView("th1" );
-        m.addObject("msg" , "請登入");
+
+        //ModelAndView m = new ModelAndView("th1"); //這個OK
+
+        boolean flag = false;
+        if(flag)
+            return method1(obj);
+            return method2(obj);
+
+    }
+    public ModelAndView method1(User obj){
+        Map<String , Object> map = new HashMap<>();
+        map.put("msg" , "請登入A");
+        map.put("myObj" , obj);
+        ModelAndView m = new ModelAndView("th1" , map);
+        return m;
+    }
+
+    public ModelAndView method2(User obj){
+        ModelAndView m = new ModelAndView();
+        m.addObject("msg" , "請登入B");
         m.addObject("myObj" , obj);
+        m.setViewName("th1");
         return m;
     }
 }
