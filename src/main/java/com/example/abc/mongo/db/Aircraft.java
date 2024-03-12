250afd74b689ce27da187194301493186cf55b89
s0911602565 <s0911602565@gmail.com>
2024-03-13 02:39:55 +0800

    20240313_0239

--- src/main/java/com/example/abc/mongo/db/Aircraft.java
+++ src/main/java/com/example/abc/mongo/db/Aircraft.java
@@ -1,10 +1,13 @@
-package com.example.abc.mongo.db;
+/*package com.example.abc.mongo.db;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Aircraft {
-    //@Id
+    @Id
+    private int id;
 
-}
+    private String name;
+
+}*/
