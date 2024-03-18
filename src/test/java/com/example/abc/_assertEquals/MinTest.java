821f386d7fab4c1f766b33c7607a684547dc8990
s0911602565 <s0911602565@gmail.com>
2024-03-18 13:46:18 +0800

    20240318_1346

--- src/test/java/com/example/abc/_assertEquals/MinTest.java
+++ src/test/java/com/example/abc/_assertEquals/MinTest.java
@@ -2,23 +2,21 @@ package com.example.abc._assertEquals;
 import com.example.abc.test._junit.j1.MyCalculator;
 
 //junit5 版本
-
+/*
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-//junit4 版本
-/*
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 */
 
+//junit4 版本
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
 public class MinTest {
-    @Tag("development")
+
     @Test
-    @DisplayName("測試相減") // 跟print("show" 一樣意思
     public void minsTow(){
         MyCalculator c = new MyCalculator();
 
--- src/test/java/com/example/abc/_assertEquals/TestJu4.java
+++ src/test/java/com/example/abc/_assertEquals/TestJu4.java
@@ -2,11 +2,10 @@ package com.example.abc._assertEquals;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//要把import 切換到junit 4 版
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
- AddTest.class ,
- MinTest.class
+ //AddTest.class , 5版
+ MinTest.class  // 4版
 })
 
 
