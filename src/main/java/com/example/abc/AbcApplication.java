60b18f1ebcd58c1b8f196abeb73de0ee9046a40e
s0911602565 <s0911602565@gmail.com>
2024-03-21 15:18:52 +0800

    202400321_1518

--- src/main/java/com/example/abc/AbcApplication.java
+++ src/main/java/com/example/abc/AbcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@ServletComponentScan //to MyWebFilter.java
 public class AbcApplication {
 
     public static void main(String[] args) {
