5310ef72ee6eee3b57217b6f204cad33a68c08f6
s0911602565 <s0911602565@gmail.com>
2024-03-24 02:33:10 +0800

    20240324_0233

--- src/main/java/com/example/abc/model/Amt.java
+++ src/main/java/com/example/abc/model/Amt.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Data
+@AllArgsConstructor //跟 @RequiredArgsConstructor 做比對
+//@RequiredArgsConstructor 要搭配@NonNull(不能null) , 被標示的欄位,建構式會按照順序
 public class Amt {
     private String id;
     private String money;
